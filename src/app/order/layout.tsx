cf27393ccc2346af37c17409515fbebac81a7f70
dhiazrionaldo <dhiazrionaldo@rtechindonesia.com>
2024-09-16 18:56:31 +0700

    trying

--- src/app/order/layout.tsx
+++ src/app/order/layout.tsx
@@ -7,9 +7,7 @@ export default function HomeLayout({children} : {children: React.ReactNode}){
         <main className="mx-5 mt-16 sm:ml-[300px] sm:mt-3">
             <Toaster />
             <Sidebar />
-            <SelectedRowProvider>
                 {children}
-            </SelectedRowProvider>
         </main>
     );
 }
