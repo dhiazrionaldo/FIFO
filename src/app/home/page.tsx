328d3c5cbd103b17bc8fb5e607f3eafa6c2dc86d
dhiazrionaldo <dhiazrionaldo@rtechindonesia.com>
2024-09-16 18:09:27 +0700

    test deploy vercel

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,9 @@
 export default async function HomePage(){
     return(
         <div>
-            <h1 className="text-3xl font-semibold capitalize">Dashbaord</h1>
+            <h1 className="text-3xl font-semibold capitalize">Dashboard</h1>
             <div className="mt-3 justify-between">
-                <p>Dashboard coy</p>
+                <p>Dashboard </p>
             </div>
         </div>
     );
