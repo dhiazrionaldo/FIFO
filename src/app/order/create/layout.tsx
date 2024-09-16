2144094868edb0da19f941a34d26c8fded72cf5a
dhiazrionaldo <dhiazrionaldo@rtechindonesia.com>
2024-09-16 18:37:43 +0700

    bugfixing selected-row-provider and create layout change

---
+++ src/app/order/create/layout.tsx
@@ -0,0 +1,15 @@
+import {Sidebar} from "@/components/sidebar"
+import { Toaster } from "react-hot-toast";
+import { SelectedRowProvider } from "../selected-row-provider";
+
+export default function HomeLayout({children} : {children: React.ReactNode}){
+    return(
+        <main className="mx-5 mt-16 sm:ml-[300px] sm:mt-3">
+            <Toaster />
+            <Sidebar />
+            <SelectedRowProvider>
+                {children}
+            </SelectedRowProvider>
+        </main>
+    );
+}
