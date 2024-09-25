212ab6136d2cc7391906890b6e32ec996f782cbe
dhiaz.rionaldo@gmail.com <dhiazrionaldo@gmail.com>
2024-09-25 15:38:28 +0700

    bugfixing dashboard for mobile view

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -25,6 +25,7 @@ import exp from "constants";
 import { DatePickerWithRange } from "./dateRange";
 import { Boxes, Loader2, Martini, PackageOpen, Search, Warehouse } from "lucide-react";
 import { Overview } from "./cost-overview";
+import { useMediaQuery } from 'usehooks-ts';
 
 
 
@@ -39,6 +40,7 @@ export default function HomePage(){ const today = new Date();
     const [expenseLounge, setExpenseLounge] = useState('');
     const [stockLounge, setStockLounge] = useState(0);
     const [stockStorage, setStockStorage] = useState(0);
+    const isDesktop = useMediaQuery("(min-width: 768px)")
 
     const GetTotalExpense = async () => {
         if (!dateRange?.from || !dateRange?.to) {
@@ -119,76 +121,160 @@ export default function HomePage(){ const today = new Date();
         GetTotalStock();
     }, []);
 
-    return(
+    if(isDesktop){
+        return(
+            <div>
+                <h1 className="text-3xl font-semibold capitalize">Dashboard</h1>
+                <div className="mt-3 justify-between">
+                <div className="hidden flex-col md:flex">
+                    <Tabs defaultValue="overview" className="space-y-4">
+                        <TabsList>
+                        <TabsTrigger value="overview">Overview</TabsTrigger>
+                        <TabsTrigger value="analytics" disabled>
+                            Analytics
+                        </TabsTrigger>
+                        <TabsTrigger value="reports" disabled>
+                            Reports
+                        </TabsTrigger>
+                        <TabsTrigger value="notifications" disabled>
+                            Notifications
+                        </TabsTrigger>
+                        </TabsList>
+                        <div className="flex flex-col md:flex-row flex-center gap-2 m-3">
+                            <DatePickerWithRange date={dateRange} setDate={setDateRange} />
+                            <Button className="text-white" onClick={GetTotalExpense}>
+                                {loading ? <Loader2 className="animate-spin" /> : <Search />}
+                            </Button>
+                        </div>
+                        <TabsContent value="overview" className="space-y-4">
+                        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                            <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                Storage Expense
+                                </CardTitle>
+                                <Warehouse className="w-5 h-5"/>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> :expenseStorage}</div>
+                            </CardContent>
+                            </Card>
+                            <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                Lounge Expense
+                                </CardTitle>
+                                <Martini className="w-5 h-5"/>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> :expenseLounge}</div>
+                            </CardContent>
+                            </Card>
+                            <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">Storage Qty</CardTitle>
+                                <Boxes className="w-5 h-5" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> :stockStorage}</div>
+                            </CardContent>
+                            </Card>
+                            <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                Lounge Qty
+                                </CardTitle>
+                                <PackageOpen className="h-5 w-5" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> :stockLounge}</div>
+                            </CardContent>
+                            </Card>
+                        </div>
+                        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                            <Card className="col-span-4">
+                            <CardHeader>
+                                <CardTitle>Overview</CardTitle>
+                            </CardHeader>
+                            <CardContent className="pl-2">
+                                <Overview />
+                            </CardContent>
+                            </Card>
+                            <Card className="col-span-3">
+                            <CardHeader>
+                                <CardTitle>Recent Sales</CardTitle>
+                                <CardDescription>
+                                You made 265 sales this month.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                {/* <RecentSales /> */}
+                            </CardContent>
+                            </Card>
+                        </div>
+                        </TabsContent>
+                    </Tabs>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
         <div>
             <h1 className="text-3xl font-semibold capitalize">Dashboard</h1>
             <div className="mt-3 justify-between">
-            <div className="hidden flex-col md:flex">
-                <Tabs defaultValue="overview" className="space-y-4">
-                    <TabsList>
-                    <TabsTrigger value="overview">Overview</TabsTrigger>
-                    <TabsTrigger value="analytics" disabled>
-                        Analytics
-                    </TabsTrigger>
-                    <TabsTrigger value="reports" disabled>
-                        Reports
-                    </TabsTrigger>
-                    <TabsTrigger value="notifications" disabled>
-                        Notifications
-                    </TabsTrigger>
-                    </TabsList>
-                    <div className="flex flex-col md:flex-row flex-center gap-2 m-3">
-                        <DatePickerWithRange date={dateRange} setDate={setDateRange} />
-                        <Button className="text-white" onClick={GetTotalExpense}>
-                            {loading ? <Loader2 className="animate-spin" /> : <Search />}
-                        </Button>
-                    </div>
-                    <TabsContent value="overview" className="space-y-4">
-                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                        <Card>
-                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                            Storage Expense
-                            </CardTitle>
-                            <Warehouse className="w-5 h-5"/>
-                        </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">{expenseStorage}</div>
-                        </CardContent>
-                        </Card>
-                        <Card>
-                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                            Lounge Expense
-                            </CardTitle>
-                            <Martini className="w-5 h-5"/>
-                        </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">{expenseLounge}</div>
-                        </CardContent>
-                        </Card>
-                        <Card>
-                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">Storage Qty</CardTitle>
-                            <Boxes className="w-5 h-5" />
-                        </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">{stockStorage}</div>
-                        </CardContent>
-                        </Card>
-                        <Card>
-                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                            Lounge Qty
-                            </CardTitle>
-                            <PackageOpen className="h-5 w-5" />
-                        </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">{stockLounge}</div>
-                        </CardContent>
-                        </Card>
-                    </div>
-                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <div className="flex flex-col md:flex-row flex-center gap-2 m-3">
+                    <DatePickerWithRange date={dateRange} setDate={setDateRange} />
+                    <Button className="text-white" onClick={GetTotalExpense}>
+                        {loading ? <Loader2 className="animate-spin" /> : <Search />}
+                    </Button>
+                </div>
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                    <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                        Storage Expense
+                        </CardTitle>
+                        <Warehouse className="w-5 h-5"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> : expenseStorage}</div>
+                    </CardContent>
+                    </Card>
+                    <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                        Lounge Expense
+                        </CardTitle>
+                        <Martini className="w-5 h-5"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> : expenseLounge}</div>
+                    </CardContent>
+                    </Card>
+                    <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">Storage Qty</CardTitle>
+                        <Boxes className="w-5 h-5" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> : stockStorage}</div>
+                    </CardContent>
+                    </Card>
+                    <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-sm font-medium">
+                        Lounge Qty
+                        </CardTitle>
+                        <PackageOpen className="h-5 w-5" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-2xl font-bold">{loading ? <Loader2 className="animate-spin"/> : stockLounge}</div>
+                    </CardContent>
+                    </Card>
+                </div>
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
                         <Card className="col-span-4">
                         <CardHeader>
                             <CardTitle>Overview</CardTitle>
@@ -209,10 +295,8 @@ export default function HomePage(){ const today = new Date();
                         </CardContent>
                         </Card>
                     </div>
-                    </TabsContent>
-                </Tabs>
                 </div>
-            </div>
         </div>
     );
+    
 }
