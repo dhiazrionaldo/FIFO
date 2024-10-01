93d0aa7267849a248bbf9e67245e4a176ba5af0d
dhiaz.rionaldo@gmail.com <dhiazrionaldo@gmail.com>
2024-10-01 19:17:57 +0700

    cache solving

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // Apply to all routes
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store',  // Disable caching for all routes
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
