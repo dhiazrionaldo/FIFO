1b38e850bdbb555ca5618e8fd45032c89059532f
dhiaz.rionaldo@gmail.com <dhiazrionaldo@gmail.com>
2024-10-01 19:25:00 +0700

    no-cache on api response header

--- src/app/api/order/getAllOrderList/route.jsx
+++ src/app/api/order/getAllOrderList/route.jsx
@@ -1,11 +1,22 @@
-import {getAllOrder} from '@/lib/db/order/order'
-import { NextResponse } from 'next/server'
-export async function GET(req){
+import { getAllOrder } from '@/lib/db/order/order';
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
     try {
         const items = await getAllOrder();
-        return NextResponse.json({items}, {status:200, headers: { 'Cache-Control': 'public, s-maxage=1' }});   
+        return NextResponse.json(
+            { items }, 
+            {
+                status: 200,
+                headers: {
+                    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    'Expires': '0',
+                    'Pragma': 'no-cache'
+                }
+            }
+        );
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({error: error.messages}, {status: 500});
-    }   
+        console.log(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 }
--- src/app/api/order/getAllStorageData/route.jsx
+++ src/app/api/order/getAllStorageData/route.jsx
@@ -3,7 +3,18 @@ import { NextResponse } from 'next/server'
 export async function GET(req){
     try {
         const items = await getAllStorage();
-        return NextResponse.json({items}, {status:200});   
+        return NextResponse.json(
+            {items}, 
+            {status:200}, 
+            {
+                status: 200,
+                headers: {
+                    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    'Expires': '0',
+                    'Pragma': 'no-cache'
+                }
+            }
+        );   
     } catch (error) {
         console.log(error)
         return NextResponse.json({error: error.messages}, {status: 500});
